334cc062ef67107c70460dcf61cd96e9aafef22d
Aaron Key <aaronmikelkey@gmail.com>
2022-11-09 18:35:58 -0500

    move apollo client to index from app, add possible unfound route page to server
    Hopeful fix to logged in page-refresh and page change errors

--- client/src/App.js
+++ client/src/App.js
@@ -10,48 +10,20 @@ import {
 	Home,
 	SignUp,
 } from './components/routes/index'
-import {
-	ApolloProvider,
-	ApolloClient,
-	InMemoryCache,
-	createHttpLink,
-} from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-
-const authLink = setContext((_, { headers }) => {
-	const token = localStorage.getItem('id_token')
-	return {
-		headers: {
-			...headers,
-			authorization: token ? `Bearer ${token}` : '',
-		},
-	}
-})
-
-const httpLink = createHttpLink({
-	uri: '/graphql',
-})
-
-const client = new ApolloClient({
-	link: authLink.concat(httpLink),
-	cache: new InMemoryCache(),
-})
 
 function App() {
 	return (
-		<ApolloProvider client={client}>
-			<Router>
-				<Header></Header>
-				<Routes>
-					<Route path="/calendar" element={<Calendar />}></Route>
-					<Route path="/schedule" element={<Schedule />}></Route>
-					<Route path="/login" element={<Login />}></Route>
-					<Route path="/" element={<Home />}></Route>
-					<Route path="/signup" element={<SignUp />}></Route>
-				</Routes>
-				<Footer />
-			</Router>
-		</ApolloProvider>
+		<Router>
+			<Header></Header>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/calendar" element={<Calendar />} />
+				<Route path="/schedule" element={<Schedule />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<SignUp />} />
+			</Routes>
+			<Footer />
+		</Router>
 	)
 }
 
--- server/server.js
+++ server/server.js
@@ -21,6 +21,9 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '../client/build')))
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
