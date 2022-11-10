f74f510b6f32f03d17fea320ab12fb7bbd4d1bb8
Erikson <chris.s.erickson@gmail.com>
2022-11-10 08:13:04 -0500

    footer css

--- client/src/components/routes/Footer.js
+++ client/src/components/routes/Footer.js
@@ -1,38 +1,43 @@
-import React from 'react'
-import { Link } from '@mui/material'
+import React from "react";
+import { Link } from "@mui/material";
 import email from "../../assets/images/email-icon.png";
 
-
 export default function footer() {
-	return (
-		<footer>
-			<Link href="/calendar" underline="hover">
-				Request Here
-			</Link>
-			<p>or find us at</p>
-			<Link
-				href="https://www.airbnb.com/rooms/655279772147097701"
-				underline="hover"
-				rel="noreferrer"
-				target = "_blank"
-			>
-				AirBNB
-			</Link>
-			<Link
-				href="https://www.vrbo.com/2879508"
-				underline="hover"
-				rel="noreferrer"
-				target="_blank"
-			>
-				VRBO
-			</Link>
-			<Link
-				href="mailto:chris@personalsunrisenc.com?subject=Personal Sunrise Inquiry"
-				underline="hover"
-				rel="noreferrer"           				
-			>
-			<img className="footer-icon" src={`${email}`} alt="email icon"></img>
-			</Link>
-		</footer>
-	)
+  return (
+    <footer>
+      <a className="footerLink">
+        <Link href="/calendar" underline="hover">
+          Calendar Request
+        </Link>
+      </a>
+      {/* <p>or find us at</p> */}
+      <a className="footerLink">
+        <Link
+          href="https://www.airbnb.com/rooms/655279772147097701"
+          underline="hover"
+          rel="noreferrer"
+          target="_blank"
+        >
+          AirBNB
+        </Link>{" "}
+      </a>
+      <a className="footerLink">
+        <Link
+          href="https://www.vrbo.com/2879508"
+          underline="hover"
+          rel="noreferrer"
+          target="_blank"
+        >
+          VRBO
+        </Link>
+      </a>
+      <Link
+        href="mailto:chris@personalsunrisenc.com?subject=Personal Sunrise Inquiry"
+        underline="hover"
+        rel="noreferrer"
+      >
+        <img className="footer-icon" src={`${email}`} alt="email icon"></img>
+      </Link>
+    </footer>
+  );
 }
